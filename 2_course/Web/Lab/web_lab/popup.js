37e23447778dc746dbd463c11869afadbab00be6
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-07-11 11:06:43 +0300

    Web added

---
+++ 2_course/Web/Lab/web_lab/popup.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("popup_form").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("popup_form").style.display = "none";
+}
