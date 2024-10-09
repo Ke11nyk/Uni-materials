798916b292b5ea476da20d3f78c1d4dcc22add5c
Ke11nyk <116598786+Ke11nyk@users.noreply.github.com>
2024-10-10 01:05:24 +0300

    rename methods

--- 3_course/Modeling_of_complex_systems/Labs/Lab_2/MCS_Lab_2.py
+++ 3_course/Modeling_of_complex_systems/Labs/Lab_2/MCS_Lab_2.py
@@ -3,13 +3,13 @@ import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 import PseudoInverseMatrixMethods as pimm
 
-def read_image(filename):
+def readImage(filename):
     # Read the .bmp input and output files as a numpy 2D matrix
     X = imageio.imread(filename)
     # Convert the image to a NumPy array
     return np.array(X)
 
-def CalculateOperator(X, Y, inversion_function, V=None, eps=1e-2, delta=10):
+def calculateOperator(X, Y, inversion_function, V=None, eps=1e-2, delta=10):
     if V is None:
         V = np.zeros((Y.shape[0], X.shape[0]))
 
@@ -23,19 +23,18 @@ def CalculateOperator(X, Y, inversion_function, V=None, eps=1e-2, delta=10):
     A = YX_pinv + VZ
     return A
 
-
-def ApplyOperator(X_, A_):
+def applyOperator(X_, A_):
     return A_ @ X_
 
-def ShowImage(X_):
+def showImage(X_):
     plt.imshow(X_, cmap='gray')
     plt.show()
 
 
-def CalculateError1(Y, Y_expected):
+def calculateError1(Y, Y_expected):
         return np.linalg.norm(Y - Y_expected, ord=1)
 
-def calculate_error(true_matrix, predicted_matrix):
+def calculateError(true_matrix, predicted_matrix):
     # Calculate the Mean Squared Error between the true matrix and the predicted matrix
     mse = np.mean((true_matrix - predicted_matrix)**2)
     rmse = np.sqrt(mse)
@@ -43,8 +42,8 @@ def calculate_error(true_matrix, predicted_matrix):
 
 
 def main():
-    X = read_image('x1.bmp')
-    Y = read_image('y4.bmp')
+    X = readImage('x1.bmp')
+    Y = readImage('y4.bmp')
 
     print('X shape:', X.shape)
     print('Y shape:', Y.shape)
@@ -55,25 +54,25 @@ def main():
     print('X:', X)
 
     # Moore-Penrose method
-    A1 = CalculateOperator(X, Y, pimm.pseudoInverseMatrix_MoorePenrose, eps=1e-60, delta=1000)
-    Y1 = ApplyOperator(X, A1)
-    ShowImage(Y1)
+    A1 = calculateOperator(X, Y, pimm.pseudoInverseMatrix_MoorePenrose, eps=1e-60, delta=1000)
+    Y1 = applyOperator(X, A1)
+    showImage(Y1)
 
     # Greville method
-    A2 = CalculateOperator(X, Y, pimm.pseudoInverseMatrix_Greville, eps=1e-60, delta=None)
-    Y2 = ApplyOperator(X, A2)
-    ShowImage(Y2)
+    A2 = calculateOperator(X, Y, pimm.pseudoInverseMatrix_Greville, eps=1e-60, delta=None)
+    Y2 = applyOperator(X, A2)
+    showImage(Y2)
 
     # SVD method
-    A3 = CalculateOperator(X, Y, pimm.pseudoInverseMatrix_SVD, eps=1e-60, delta=None)
-    Y3 = ApplyOperator(X, A3)
-    ShowImage(Y3)
+    A3 = calculateOperator(X, Y, pimm.pseudoInverseMatrix_SVD, eps=1e-60, delta=None)
+    Y3 = applyOperator(X, A3)
+    showImage(Y3)
 
-    print('Error 1 Moore-Penrose:', CalculateError1(Y, Y1))
-    print('Error 1 Greville:', CalculateError1(Y, Y2))
+    print('Error 1 Moore-Penrose:', calculateError1(Y, Y1))
+    print('Error 1 Greville:', calculateError1(Y, Y2))
 
-    print('MSE, RMSE Moore-Penrose:', calculate_error(Y, Y1))
-    print('MSE, RMSE Greville:', calculate_error(Y, Y2))
+    print('MSE, RMSE Moore-Penrose:', calculateError(Y, Y1))
+    print('MSE, RMSE Greville:', calculateError(Y, Y2))
 
 if __name__ == "__main__":
     main()
